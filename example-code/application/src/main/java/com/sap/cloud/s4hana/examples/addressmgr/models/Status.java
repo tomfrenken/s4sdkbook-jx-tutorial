4812469aeb22f86dbd5dea691f4c6feefa86faed
tomfrenken <tom.frenken@sap.com>
2020-01-17 09:52:46 +0100

    reset example
    Signed-off-by: tomfrenken <tom.frenken@sap.com>

--- example-code/application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/models/Status.java
+++
@@ -1,5 +0,0 @@
-package com.sap.cloud.s4hana.examples.addressmgr.models;
-
-public enum Status {
-    NEW, APPROVED, REJECTED
-}
--- example-code/application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/service/TenantProvisioningService.java
+++
@@ -1,7 +0,0 @@
-package com.sap.cloud.s4hana.examples.addressmgr.service;
-
-public interface TenantProvisioningService {
-    void subscribeTenant(String tenantId);
-
-    void unsubscribeTenant(String tenantId);
-}
